49048f0f08b30cdd97b2f8ec12a76a629cd87ed7
macmiller87 <macamagolf@gmail.com>
2023-05-05 11:10:55 -0300

    Refactor: route getUsersAndTeams

--- src/routes/user/GetUserAndTeams.js
+++ src/routes/user/GetUserAndTeams.js
@@ -2,10 +2,22 @@ const arrayUsers = require("../../utils/ArrayUsers");
 
 async function getUsersAndTeams(request, response) {
 
-    response.end(JSON.stringify({
-        users: arrayUsers
-    }));
+    const url = request.url;
+    const splitUrl = url.split("/");
+    const userId = splitUrl[2];
 
+    const findUserById = await arrayUsers.find((item) => item.user_id === userId);
+
+    if(findUserById) {
+        response.writeHead(200);
+        return response.end(JSON.stringify({
+            user: findUserById
+        }));
+
+    }else {
+        response.writeHead(404);
+        return response.end(JSON.stringify({ message: "User Not Found !" }));
+    }
 }
 
 module.exports = getUsersAndTeams;
